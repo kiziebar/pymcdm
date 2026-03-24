2733a475b8087136f674d30d0906a84b794caedc
kiziebar <kiziub@gmail.com>
2026-03-24 12:33:57 +0100

    init update

--- pymcdm/weights/subjective/__init__.py
+++ pymcdm/weights/subjective/__init__.py
@@ -1,9 +1,45 @@
 from .ahp import AHP
 from .rancom import RANCOM
 from .pairwise_weights_base import PairwiseWeightsBase
+from .subjective_weights_base import SubjectiveWeightsBase
+from .ordered_criteria_base import OrderedCriteriaWeightsBase
+from .grouped_ranks_base import GroupedRanksBase
+from .optimization_base import OptimizationWeightsBase
+from .decision_matrix_base import DecisionMatrixWeightsBase
+from .swara import SWARA
+from .swan import SWAN
+from .piprecia import PIPRECIA
+from .fucom import FUCOM
+from .bwm import BWM
+from .bcm import BCM
+from .lbwa import LBWA
+from .simos import SIMOS
+from .srf import SRF
+from .vimm import VIMM
+from .cobrac import COBRAC
+from .itara import ITARA
+from .owcm import OWCM
 
 __all__ = [
     'AHP',
     'RANCOM',
-    'PairwiseWeightsBase'
+    'PairwiseWeightsBase',
+    'SubjectiveWeightsBase',
+    'OrderedCriteriaWeightsBase',
+    'GroupedRanksBase',
+    'OptimizationWeightsBase',
+    'DecisionMatrixWeightsBase',
+    'SWARA',
+    'SWAN',
+    'PIPRECIA',
+    'FUCOM',
+    'BWM',
+    'BCM',
+    'LBWA',
+    'SIMOS',
+    'SRF',
+    'VIMM',
+    'COBRAC',
+    'ITARA',
+    'OWCM',
 ]
